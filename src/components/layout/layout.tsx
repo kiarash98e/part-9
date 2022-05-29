1a1c9eaa1837ac1d039d76fb4f20050871838dd5
kiarash98e <kiarashsafaei0@gmail.com>
2022-05-29 23:31:48 +0430

    add layout project

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import MobileMenu from './mobileMenu/mobileMenu'
+import {Outlet} from 'react-router-dom'
+
+const Layout:React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen bg-heading mx-auto">
+
+        <div>
+            <Outlet />
+        </div>
+        <div className='md:mx-auto'>
+            <MobileMenu />
+        </div>
+    </div>
+  )
+}
+
+export default Layout
