614fa349455853f38dc15fafeab7830862f3df53
kiarash98e <kiarashsafaei0@gmail.com>
2022-05-29 23:33:27 +0430

    create pages routing

---
+++ src/pages/calling/calling.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Calling:React.FC = () => {
+  
+return (
+    <div>Calling</div>
+  )
+}
+
+export default Calling
---
+++ src/pages/favorites/favorites.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Favorites:React.FC = () => {
+    return (
+        <div className='text-white'>
+            favorites
+        </div>
+    )
+}
+
+export default Favorites
