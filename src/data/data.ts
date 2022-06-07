cde980278485d639d84e0407123d20bfe8d36cfa
kiarash98e <kiarashsafaei0@gmail.com>
2022-06-07 23:54:38 +0430

    data fake

---
+++ src/data/data.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-octal */
+import {IData} from '../typpes'
+
+const contactsData : IData[] = [
+    {
+        id: 1,
+        name:"mmd akbry",
+        phone: "09116645678",
+    },
+    {
+        id: 2,
+        name:"reza ziaroe",
+        phone:" 09112007669",
+    },
+    {
+        id: 3,
+        name:"#rez",
+        phone: "09157649907",
+    },
+    {
+        id: 4,
+        name:"kiarash",
+        phone: "09114141381",
+    },
+]
+
+export default contactsData
