a366099c6a08da8a5963da366f4bf083e2fbbe37
kiarash98e <kiarashsafaei0@gmail.com>
2022-06-07 23:51:03 +0430

    add layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import MobileMenu from './mobileMenu/mobileMenu'
-import {Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 const Layout:React.FC = () => {
+
+
   return (
     <div className="flex flex-col min-h-screen bg-heading mx-auto">
 
-        <div>
+        <div className='md:mx-auto'>
             <Outlet />
         </div>
         <div className='md:mx-auto'>
